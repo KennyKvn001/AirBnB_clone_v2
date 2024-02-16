4388591b756ad6dde6004d40a56b37133d233e3b
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-16 16:16:54 +0200

    Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
-""" 0x03. AirBnB clone - Deploy static, task 4. Keep it clean!
+""" alu-Airbnb_clone_v2 - Deploy static, task 4. Keep it clean!
 """
-from fabric.api import env, run, local
+from fabric.api import env, run, lcd, cd
+from fabric.operations import local
 
 env.hosts = ["204.236.196.88", "34.224.218.238"]
 env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """Deletes out-of-date .tgz archives of `web_static/`, both local and
-    remote, created by `do_pack`.
-
-    Args:
-       (str): number of archives, including the most recent, to keep. If 0 or
-            1, only the most recent is kept.
-
     """
-    number = eval(number)
-    if number == 0:
-        number = 1
-    # local removal of tgz archives
-    pipe = " ".join(
-        (
-            "ls -t versions/ | tail -n +{:d} |".format(number + 1),
-            'sed -e "s/^/versions\//" |',
-            "xargs rm -rf",
-        )
-    )
-    local(pipe)
+    Deletes out-of-date archives
+    """
+    try:
+        number = int(number)
+    except ValueError:
+        print("Error: Number must be an integer")
+        return False
 
-    # run on server to remove outdated directories in web_static/releases/
-    pipe = " ".join(
-        (
-            "ls -t /data/web_static/releases/ |",
-            'sed -e "/test/d" |',
-            "tail -n +{:d} |".format(number + 1),
-            'sed -e "s/^/\/data\/web_static\/releases\//" |',
-            "xargs rm -rf",
-        )
-    )
-    run(pipe)
+    if number < 0:
+        print("Error: Number must be a positive integer")
+        return False
+
+    with lcd("versions"):
+        archives_local = local("ls -t | grep '.tgz'", capture=True).split("\n")
+
+        archives_to_keep_local = archives_local[:number]
+
+        for archive in archives_local:
+            if archive not in archives_to_keep_local:
+                local("rm -f {}".format(archive))
+
+    with cd("/data/web_static/releases"):
+        archives_remote = run("ls -t | grep '.tgz'").split("\n")
+
+        archives_to_keep_remote = archives_remote[:number]
+
+        for archive in archives_remote:
+            if archive not in archives_to_keep_remote:
+                run("rm -f {}".format(archive))
+
+    return True
