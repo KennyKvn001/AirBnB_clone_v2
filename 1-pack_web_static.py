a90c1ab7f0c734145403d913932283b24436ddae
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-16 11:27:41 +0200

    updates

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,18 +3,15 @@
 
 from fabric.api import local
 from datetime import datetime
-import os
 
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder"""
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     try:
-        if not os.path.exists("versions"):
-            os.mkdir("versions")
         filename = "versions/web_static_{}.tgz".format(date)
         local("mkdir -p versions")
-        local("tar -cvzf {} web_static".format(filename))
+        local(f"tar -cvzf {filename}  web_static")
         return filename
     except:
         return None
