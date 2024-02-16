109fb2c5c5c6f45aa6a60562c91852b7c7daf294
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-16 16:24:56 +0200

    Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python3
 """ alu-Airbnb_clone_v2 - Deploy static, task 4. Keep it clean!
 """
-from fabric.api import env, run, lcd, cd
-from fabric.operations import local
+from fabric.api import env, run, cd, sudo
 
 env.hosts = ["204.236.196.88", "34.224.218.238"]
 env.user = "ubuntu"
 
 
-def do_clean(number=0):
+# Replace with the number of archives to keep
+NUMBER_TO_KEEP = 2
+
+
+def do_clean(number=NUMBER_TO_KEEP):
     """
-    Deletes out-of-date archives
+    Deletes out-of-date archives locally and on web servers.
+
+    Args:
+        number: Number of archives (including the most recent) to keep.
     """
-    try:
-        number = int(number)
-    except ValueError:
-        print("Error: Number must be an integer")
-        return False
 
-    if number < 0:
-        print("Error: Number must be a positive integer")
-        return False
+    with cd("versions"):
+        # Get a list of archive filenames sorted by reverse creation time
+        archives = sorted(run("ls -rt").split(), reverse=True)
 
-    with lcd("versions"):
-        archives_local = local("ls -t | grep '.tgz'", capture=True).split("\n")
+        # Delete all but the first `number` archives
+        for archive in archives[number:]:
+            run("rm %s" % archive)
 
-        archives_to_keep_local = archives_local[:number]
+    # Run cleanup on both web servers
+    for host in env.hosts:
+        with cd("%s/releases" % host):
+            # Connect to the server and run do_clean with the same number
+            sudo("fab -H %s do_clean:%s" % (host, number))
 
-        for archive in archives_local:
-            if archive not in archives_to_keep_local:
-                local("rm -f {}".format(archive))
 
-    with cd("/data/web_static/releases"):
-        archives_remote = run("ls -t | grep '.tgz'").split("\n")
-
-        archives_to_keep_remote = archives_remote[:number]
-
-        for archive in archives_remote:
-            if archive not in archives_to_keep_remote:
-                run("rm -f {}".format(archive))
-
-    return True
+if __name__ == "__main__":
+    do_clean()
