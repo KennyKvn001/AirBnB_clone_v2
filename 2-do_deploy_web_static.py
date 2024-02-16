bf802c9aaf35f5bec1fe51b2a051ff6109aa0b53
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-16 16:50:28 +0200

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from fabric.api import env, put, run, cd
+from fabric.api import env, put, run
 import os.path
 
 
@@ -11,6 +11,7 @@ env.key_filename = "~/.ssh/id_rsa"
 def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
     if not os.path.exists(archive_path):
+        print("Archive does not exist:", archive_path)
         return False
 
     try:
@@ -21,9 +22,8 @@ def do_deploy(archive_path):
         archive_name = os.path.basename(archive_path)
         archive_name_no_ext = os.path.splitext(archive_name)[0]
 
-        # Uncompress the archive to /data/web_static/releases/<archive filename
-        # without extension>
-        run("mkdir -p /data/web_static/releases/{}".format(archive_name_no_ext))
+        # Uncompress the archive to /data/web_static/releases/<archive filename without extension>
+        run("mkdir -p /data/web_static/releases/{}/".format(archive_name_no_ext))
         run(
             "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
                 archive_name, archive_name_no_ext
@@ -50,7 +50,7 @@ def do_deploy(archive_path):
 
         # Create a new symbolic link
         run(
-            "ln -s /data/web_static/releases/{} /data/web_static/current".format(
+            "ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
                 archive_name_no_ext
             )
         )
