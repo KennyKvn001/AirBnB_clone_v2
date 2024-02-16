d76623b1197ee848a224914f2a2e6ac173839998
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-16 17:48:15 +0200

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from fabric.api import env, put, run
+import re
 import os
+from os.path import exists, isdir
 
 
 env.user = "ubuntu"
@@ -9,79 +11,34 @@ env.key_filename = "~/.ssh/school"
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
-        print("Archive does not exist:", archive_path)
+    """
+    Distributes archive to web servers
+    """
+    if not exists(archive_path):
         return False
+    put(archive_path, "/tmp/")
+    filename = re.search(r"[^/]+$", archive_path).group(0)
+    folder = "/data/web_static/releases/{}".format(os.path.splitext(filename)[0])
 
-    try:
-        # Upload the archive to the /tmp/ directory of the web server
-        print("Uploading archive...")
-        put(archive_path, "/tmp/")
-        print("Archive uploaded successfully")
+    if not exists(folder):
+        run("mkdir -p {}".format(folder))
 
-        # Extract archive filename
-        archive_name = os.path.basename(archive_path)
-        archive_name_no_ext = os.path.splitext(archive_name)[0]
+    run("tar -xzf /tmp/{} -C {}".format(filename, folder))
 
-        # Uncompress the archive to /data/web_static/releases/<archive filename
-        # without extension>
-        print("Extracting archive...")
-        run("mkdir -p /data/web_static/releases/{}/".format(archive_name_no_ext))
-        run(
-            "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-                archive_name, archive_name_no_ext
-            )
-        )
-        print("Archive extracted successfully")
+    run("rm /tmp/{}".format(filename))
 
-        # Delete the archive from the web server
-        print("Deleting archive...")
-        run("rm /tmp/{}".format(archive_name))
-        print("Archive deleted successfully")
+    run("mv {}/web_static/* {}".format(folder, folder))
 
-        # Move contents to proper location
-        print("Moving contents to proper location...")
-        run(
-            "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(
-                archive_name_no_ext, archive_name_no_ext
-            )
-        )
+    run("rm -rf {}/web_static".format(folder))
 
-        # Remove the now empty directory
-        print("Removing empty directory...")
-        run(
-            "rm -rf /data/web_static/releases/{}/web_static".format(archive_name_no_ext)
-        )
+    run("rm -rf /data/web_static/current")
 
-        # Check if my_index.html exists in the expected location
-        index_path = "/data/web_static/releases/{}/my_index.html".format(
-            archive_name_no_ext
-        )
-        index_exists = run(
-            "test -f {}".format(index_path),
-            warn=True).succeeded
+    run("ln -s {} /data/web_static/current".format(folder))
 
-        if index_exists:
-            print("my_index.html found at:", index_path)
-        else:
-            print("ERROR: my_index.html not found at:", index_path)
-            return False
+    if not isdir("/var/www/html/hbnb_static"):
+        run("sudo mkdir -p /var/www/html/hbnb_static")
 
-        # Delete the symbolic link /data/web_static/current from the web server
-        print("Deleting current symlink...")
-        run("rm -rf /data/web_static/current")
+    run("sudo cp -r /data/web_static/current/* /var/www/html/hbnb_static/")
 
-        # Create a new symbolic link
-        print("Creating new symlink...")
-        run(
-            "ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
-                archive_name_no_ext
-            )
-        )
-
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        print("Deployment failed:", e)
-        return False
+    print("New version deployed!")
+    return True
