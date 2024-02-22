9012335519841dcfa2caf4daeacd49ca59bd1c70
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-22 22:58:12 +0200

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -67,7 +67,10 @@ def html_odd_or_even(n):
     substitute text to display if int is odd or even
     """
     odd_or_even = "even" if (n % 2 == 0) else "odd"
-    return render_template("6-number_odd_or_even.html", n=n, odd_or_even=odd_or_even)
+    return render_template(
+        "6-number_odd_or_even.html",
+        n=n,
+        odd_or_even=odd_or_even)
 
 
 if __name__ == "__main__":
